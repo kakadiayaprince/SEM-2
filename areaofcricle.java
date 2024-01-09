91b4f8f2e70a3b616917de97873617301bcbb6a8
kakadiayaprince <156084541+kakadiayaprince@users.noreply.github.com>
2024-01-09 17:26:18 +0530

    Add files via upload

---
+++ areaofcricle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class areaofcricle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please enter a radius of cricle:");
+        double a=sc.nextDouble();
+        double b=(22/7.0)*a*a;
+        System.out.println(b);
+
+
+
+    }
+    
+}
