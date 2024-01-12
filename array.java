337cb93ea40fd28adcf9bd0bacea744a3c5e2051
kakadiayaprince <156084541+kakadiayaprince@users.noreply.github.com>
2024-01-12 12:06:33 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,15 @@
+ import java.util.*;
+ public class array {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int[] a=new int[5];
+       for(int i=0;i<a.length;i++){
+        a[i]=sc.nextInt();
+       }
+       for(int j=a.length-1;j>=0;j--){
+        
+        System.out.println(a[4]);
+       
+    } 
+    }
+}
