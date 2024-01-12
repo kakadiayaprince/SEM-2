c0ae814dd16faa555941d0b19f96eb4c38c7a312
kakadiayaprince <156084541+kakadiayaprince@users.noreply.github.com>
2024-01-12 13:11:55 +0530

    Add files via upload

---
+++ clatoph.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class clatoph {
+public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please enter  an Fahrenheit:");
+        double a=sc.nextDouble();
+        double b=((a-32)*(5))/9;
+        System.out.println(b);
+
+    }
+    
+}
+
---
+++ comandline.java
@@ -0,0 +1,8 @@
+
+public class comandline {
+    public static void main(String[] args) {
+        System.out.print("enter int:");
+        int a=Integer
+    }
+    
+}
