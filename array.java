97a98c5fd050a8b50949359fe39402bd2c61b0ff
kakadiayaprince <156084541+kakadiayaprince@users.noreply.github.com>
2024-01-17 07:48:50 +0530

    Update array.java

--- array.java
+++ array.java
@@ -3,13 +3,14 @@
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int[] a=new int[5];
+     int k=1;
        for(int i=0;i<a.length;i++){
         a[i]=sc.nextInt();
        }
        for(int j=a.length-1;j>=0;j--){
         
-        System.out.println(a[4]);
-       
+        System.out.println(a[4-k]);
+       k++;
     } 
     }
 }
